888a7cf9202f48c140b0df352c893238c5d04aff
Liam Tat Tze Tey <liamtattze.tey@ocado.com>
2024-01-09 23:57:56 +0000

    refactor-lsp-and-jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,44 +1,7 @@
-local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc, noremap = true, silent = true })
-    end
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<leader>k', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
-end
-
 local jdtls = require('jdtls')
-local jdtls_setup = require('jdtls.setup')
+local utils_lsp = require('utils.lsp')
 
 local home = os.getenv('HOME')
-
-local root_markers = { '.git', 'mvnw', 'gradlew' }
-local root_dir = jdtls_setup.find_root(root_markers)
 
 local project_name = vim.fn.fnamemodify(root_dir, ':p:h:t')
 local workspace_dir = home .. '/.cache/jdtls/workspace' .. project_name
@@ -143,12 +106,12 @@ config.settings = {
     },
 }
 
-config.on_attach = on_attach
+config.on_attach = utils_lsp.on_attach
 
 config.on_init = function(client, _)
     client.notify('workspace/didChangeConfiguration', { settings = config.settings })
 end
 
-config.rootdir = root_dir
+config.rootdir = utils_lsp.get_root('.git', 'mvnw', 'gradlew')
 
 jdtls.start_or_attach(config)
